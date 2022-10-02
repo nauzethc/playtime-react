8dee11c440aa8c2b599ddb6db7272453c42d06e5
Nauzet Hernández <nauzet@minos.local>
2022-10-02 12:26:23 +0100

    Added next/link component

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+
 export default function Logo ({ className }) {
   return (
     <div className={`logo ${className || ''}`}>
-      <a href='/' alt='PlayTime'>
-        <span className='text-purple-500'>Play</span>
-        <span className='font-bold'>Time</span>
-      </a>
+      <Link href="/">
+        <a alt="PlayTime">
+          <span className="text-purple-500">Play</span>
+          <span className="font-bold">Time</span>
+        </a>
+      </Link>
     </div>
   )
 }
